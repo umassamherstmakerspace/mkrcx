51db470bc2b23a086defb82ad03792843b06fa36
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2024-01-23 21:01:16 -0500

    add error page

---
+++ src/mkrcx-frontend/src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { Heading, P } from 'flowbite-svelte';
+    import { page } from '$app/stores';
+
+    $: console.log($page.error);
+</script>
+
+<div class="flex flex-col items-center justify-center w-full h-full space-y-10">
+    <Heading level="1" size="xl" class="text-center mb-4">Error {$page?.status}</Heading>
+    <P>{$page?.error?.message}</P>
+</div>
