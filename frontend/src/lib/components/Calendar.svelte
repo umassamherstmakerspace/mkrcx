61ac93ef235815aa6e4de1db20d68f1785d08f76
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2024-03-25 00:00:55 -0400

    fixed calendar color scheme

--- frontend/src/lib/components/Calendar.svelte
+++ frontend/src/lib/components/Calendar.svelte
@@ -3,6 +3,7 @@
 	import dayGridPlugin from '@fullcalendar/daygrid';
 	import timeGridPlugin from '@fullcalendar/timegrid';
 	import listPlugin from '@fullcalendar/list';
+	import './calendar.pcss'
 
 	export let url: string;
 
