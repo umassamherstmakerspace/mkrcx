96073ae28a96a96fdc8c130fbf5d5f0a87ff55b0
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2024-03-27 00:59:03 -0400

    card link progress

--- clients/card-helper/src/reader.rs
+++ clients/card-helper/src/reader.rs
@@ -1,54 +1,51 @@
-use std::{ffi::CStr, future::Future};
+use pcsc::*;
 
-use pcsc;
+use std::{ffi::CString, sync::Arc};
+use tokio::sync::Mutex;
 
-struct CardFuture<'a> {
-    card: pcsc::Card,
-    rapdu_buf: [u8; pcsc::MAX_BUFFER_SIZE],
-    apdu: &'a [u8],
+pub struct AsyncPcsc {
+    ctx: Arc<Mutex<pcsc::Context>>,
 }
 
-impl Future for CardFuture<'_> {
-    type Output = Result<&'static [u8], pcsc::Error>;
+impl AsyncPcsc {
+    pub async fn establish() -> Result<Self, pcsc::Error> {
+        let context = pcsc::Context::establish(Scope::User)?;
+        Ok(AsyncPcsc {
+            ctx: Arc::new(Mutex::new(context)),
+        })
+    }
 
-    fn poll(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context) -> std::task::Poll<Self::Output> {
-        let this = self.get_mut();
-        match this.card.transmit(this.apdu, &mut this.rapdu_buf) {
-            Ok(rapdu) => {
-                let rapdu = &rapdu[..rapdu.len() - 2];
-                std::task::Poll::Ready(Ok(rapdu))
-            }
-            Err(pcsc::Error::NoSmartcard) => {
-                std::task::Poll::Ready(Err(pcsc::Error::NoSmartcard))
-            }
-            Err(err) => {
-                std::task::Poll::Ready(Err(err))
+    pub async fn list_readers(&self) -> Result<Vec<CString>, pcsc::Error> {
+        let context = self.ctx.lock().await;
+        let readers = context.list_readers_owned()?;
+        Ok(readers)
+    }
+
+    pub async fn try_connect(&self, reader: &CString) -> Result<pcsc::Card, pcsc::Error> {
+        let context = self.ctx.lock().await;
+        let card = context.connect(reader, ShareMode::Shared, Protocols::ANY)?;
+        Ok(card)
+    }
+
+    pub async fn connect(&self, reader: &CString) -> Result<pcsc::Card, pcsc::Error> {
+        let context = self.ctx.lock().await;
+        loop {
+            match context.connect(reader, ShareMode::Shared, Protocols::ANY) {
+                Ok(card) => return Ok(card),
+                Err(pcsc::Error::NoSmartcard) => {
+                    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+                }
+                Err(err) => {
+                    return Err(err);
+                }
             }
         }
     }
-}
 
-
-pub struct Reader<'a> {
-    context: pcsc::Context,
-    reader: &'a CStr,
-}
-
-impl Reader<'_> {
-    pub fn new(reader: &CStr) -> Result<Reader, pcsc::Error> {
-        let context = pcsc::Context::establish(pcsc::Scope::User)?;
-        Ok(Reader { context, reader })
-    }
-
-    pub fn get_first_reader<'a>() -> Result<&'a CStr, pcsc::Error> {
-        let context = pcsc::Context::establish(pcsc::Scope::User)?;
-        let mut readers_buf = [0; 2048];
-        let mut readers = context.list_readers(&mut readers_buf)?;
-        let reader = readers.next().ok_or(pcsc::Error::NoReadersAvailable)?;
-        Ok(reader)
-    }
-
-    async fn connect(&self) -> Result<pcsc::Card, pcsc::Error> {
-        self.context.connect(self.reader, pcsc::ShareMode::Shared, pcsc::Protocols::ANY)
+    pub async fn transmit(&self, card: &pcsc::Card, apdu: &[u8]) -> Result<Vec<u8>, pcsc::Error> {
+        let _ = self.ctx.lock().await;
+        let mut rapdu_buf = [0; MAX_BUFFER_SIZE];
+        let rapdu = card.transmit(apdu, &mut rapdu_buf)?;
+        Ok(rapdu.to_vec())
     }
 }
