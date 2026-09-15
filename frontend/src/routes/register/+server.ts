a7893b8309e3ec74ae2edd103d63192e2204fc6c
Shira <shira.xiv@gmail.com>
2026-09-15 16:47:56 -0400

    Make registration safe for returning members (#37)
    * Make registration safe for returning members
    
    * Use explicit homepage destinations on registration page
    
    ---------
    
    Co-authored-by: Shira Epstein <shirae@umass.edu>

--- frontend/src/routes/register/+server.ts
+++
@@ -1,5 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
-import { registrationAccountChooserUrl } from '$lib/registration';
-
-export const GET: RequestHandler = () => redirect(302, registrationAccountChooserUrl());
